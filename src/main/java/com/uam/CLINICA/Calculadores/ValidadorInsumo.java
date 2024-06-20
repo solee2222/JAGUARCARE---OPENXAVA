907fd6db441160326734b3c9d244b3f5e62fa34a
Shemely Perez <perezshm2@gmail.com>
2024-06-20 15:50:17 -0600

    Entidad Insumo y Validador

--- src/main/java/com/uam/CLINICA/Calculadores/ValidadorInsumo.java
+++ src/main/java/com/uam/CLINICA/Calculadores/ValidadorInsumo.java
@@ -1,5 +1,23 @@
 package com.uam.CLINICA.Calculadores;
 
-public class ValidadorInsumo {
+import java.util.*;
+
+import org.openxava.util.*;
+import org.openxava.validators.*;
+
+import com.uam.CLINICA.model.*;
+
+import service.*;
+
+public class ValidadorInsumo implements IPropertyValidator {
+	
+	  @Override
+	    public void validate(Messages messages, Object o, String s, String s1) throws Exception {
+	        IDAO dao = new ImplDAO();
+	        List<Insumo> lista = dao.get("Insumo.findByNombre", Insumo.class, String.valueOf(o));
+	        if (lista != null && lista.size() > 0) {
+	            messages.add("El nombre del insumo ya existe");
+	        }
+	    }
 
 }
--- src/main/java/com/uam/CLINICA/model/Insumo.java
+++ src/main/java/com/uam/CLINICA/model/Insumo.java
@@ -1,5 +1,41 @@
 package com.uam.CLINICA.model;
 
-public class Insumo {
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import com.uam.CLINICA.Calculadores.*;
+import com.uam.CLINICA.exceptions.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+public class Insumo extends Identificable{
+	
+	@PropertyValidator(value= ValidadorInsumo.class)
+	private String nombreInsumo;
+	
+	@Column
+	private String descripcion;
+	
+	@Column
+	private Integer cantidadDisponible;
+	
+	@Column
+	private Integer cantidadMinima;
+	
+	
+	@PrePersist
+    @PreUpdate
+    public void verificarCantidadMinima() {
+        if (cantidadDisponible != null && cantidadMinima != null) {
+            if (cantidadDisponible <= cantidadMinima) {
+                throw new RefillException(
+                        "La cantidad disponible de '" + nombreInsumo + "' está cerca o por debajo de la cantidad mínima. Rellene."
+                );
+            }
+        }
+    }
 
 }
